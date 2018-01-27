f60c0244f61e59bc579dc94c9a2542d8f71b602f
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-01-27 10:05:55 +0530

    Add files via upload

---
+++ snakenladder.py
@@ -0,0 +1,52 @@
+#Program for snake n ladder game.
+import random
+count=0
+print("Welcome ")
+print("Your current position is",count)
+while(count<100):
+     i=random.randint(1,6)
+     j=input("Press enter to continue")
+     count=count+i
+     print("Your current position is",count)
+     if count==8:
+         count=37
+         print("You climbed the ladder")
+     elif count==11:
+           count=2
+           print("Snake bite")
+     elif count==13:
+           count=34
+           print("You climbed the ladder")
+     elif count==25:
+           count=4
+           print("Snake bite")
+     elif count==38:
+           count=9
+           print("Snake bite")
+     elif count==40:
+           count=68
+           print("You climbed the ladder")
+     elif count==52:
+           count=81
+           print("You climbed the ladder")
+     elif count==65:
+           count=46
+           print("Snake bite")
+     elif count==76:
+           count=97
+           print("You climbed the ladder")
+     elif count==89:
+           count=70
+           print("Snake bite")
+     elif count==93:
+           count=64
+           print("Snake bite")
+     elif count==100:
+           count=100
+           print("YOU WON")
+           break
+while(count>100):
+    print("try again")
+    break
+
+     
