f70c11f5cf8a49b7d8b119e211f5f16aa6ddc5d8
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-01-27 08:36:06 +0530

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,13 @@
+#Program for addition, subtraction, multiplication, division, modulous.
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
