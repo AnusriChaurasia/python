1744df1d05df7202c5bd0a2edd21a1270cca1cda
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-02-22 14:34:10 +0530

    Add files via upload

---
+++ counting_back.py
@@ -0,0 +1,5 @@
+""" counting back using for loop"""
+for x in range(21):
+    while(x<21):            #here x should be less than 21 since x is in range of 21 
+        print(20-x) 
+        break               # breaks the loop as without breakit continues to execute again and again
