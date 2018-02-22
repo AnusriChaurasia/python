3f741fd4ec54deaa51dac9bb159834b54969f28d
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-02-22 14:25:50 +0530

    Add files via upload

---
+++ relational.py
@@ -0,0 +1,19 @@
+""" using relationl operators"""
+print("the relational operation on x and y will be, where x=165 and y=354:")
+x=165
+y=354
+""" greater than, smaller than, equal to, not equal to, greater tha equal to,
+smaller than equal to respectively"""
+print("x>y is ",x>y)
+print("x<y is ",x<y)
+print("x==y is ",x==y)
+print("x!=y is",x!=y)
+print("x>=y is ",x>=y)
+print("x<=y is ",x<=y)
+""" using logical operators"""
+print("the logical operations on x and y will be as follows, where x is true and y is false:")
+x=True
+y=False
+print("x and y is ",x and y)
+print("x or y is ",x or y)
+print("not x is",not x)
