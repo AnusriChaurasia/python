712d803219f4640d2484098501e9ba439b1b54e4
AnusriChaurasia <35715688+AnusriChaurasia@users.noreply.github.com>
2018-02-22 14:43:32 +0530

    Update counting_back.py

--- counting_back.py
+++ counting_back.py
@@ -3,3 +3,8 @@ for x in range(21):
     while(x<21):            #here x should be less than 21 since x is in range of 21 
         print(20-x) 
         break               # breaks the loop as without breakit continues to execute again and again
+""" counting back using only while loop"""
+num=20
+while(num>=0):              # considers the value of num till it is greater than or equal to 0
+    print(num)              
+    num-=1                  # lessens the value of num after every execution
